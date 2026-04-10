75d34d35505cb094edb2db40c351bd1473440f77
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-10 09:10:15 -0300

    Update _atividade_3.java

--- _atividade_3.java
+++ _atividade_3.java
@@ -1,21 +1,23 @@
-package Java_Lista_1_Se_e_Enquanto;
+package Java_Lista_2_Se_e_Mod;
 
 import java.util.Scanner;
 
-public class _atividade_11 {
+public class _atividade_3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int numb, result;
+        double numb1, result;
 
-        System.out.println("Insira um número para saber se ele é divisível por 5");
-        numb = sc.nextInt();
-        result = numb % 5;
+        System.out.println("Insira um número para saber se ele é múltiplo de 5");
+        numb1 = sc.nextDouble();
+
+        result = numb1 % 5;
 
         if (result == 0) {
-            System.out.println("Este número é divisível por 5");
+            System.out.println("Ele é múltiplo de 5");
         }
         else {
-            System.out.println("Este número não é divisível por 5");
+            System.out.println("Ele não é múltiplo de 5");
         }
     }
 }
+
