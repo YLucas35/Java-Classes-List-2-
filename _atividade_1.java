072446607c46fdaf1284106ddee3274479c70c8e
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-10 08:34:37 -0300

    Create _atividade_1.java

---
+++ _atividade_1.java
@@ -0,0 +1,22 @@
+package Java_Lista_2_Se_e_Mod;
+
+import java.util.Scanner;
+
+public class _atividade_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double numb1, par, impar, result;
+
+        System.out.println("Insira um número para saber se ele é par ou impar");
+        numb1 = sc.nextDouble();
+
+        result = numb1 % 2;
+
+        if (result == 0) {
+            System.out.println("É par");
+        }
+        else {
+            System.out.println("É impar");
+        }
+    }
+}
