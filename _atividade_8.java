3e69d2c119fe91f602d358fb6a6e1357a2c0a54f
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-10 10:58:44 -0300

    Create _atividade_8.java

---
+++ _atividade_8.java
@@ -0,0 +1,29 @@
+package Java_Lista_2_Se_e_Mod;
+
+import java.util.Scanner;
+
+public class _atividad_8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double numb1, numb2, result;
+
+        System.out.println("Insira um número para ser o dividendo da operação");
+        numb1 = sc.nextDouble();
+
+        System.out.println("Insira um número para ser o divisor da operação");
+        numb2 = sc.nextDouble();
+
+        if (numb2 == 0) {
+            System.out.println("Erro: Não é possível dividir por zero!");
+        } else {
+            // Se não for zero, aí sim calculamos o resto
+            result = numb1 % numb2;
+
+            if (result == 0) {
+                System.out.println("Divisão exata");
+            } else {
+                System.out.println("O resto da divisão é: " + result);
+            }
+        }
+    }
+}
